a25bbc94e653c27b9a05e5a5a977ad3e0cee33d9
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-05-13 14:25:59 +0200

    Setup nameserver

---
+++ backend/nameserver/src/main/java/com/dhbw/wwi19b2/whatamiwatching/nameserver/NameserverApplication.java
@@ -0,0 +1,15 @@
+package com.dhbw.wwi19b2.whatamiwatching.nameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NameserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameserverApplication.class, args);
+	}
+
+}
