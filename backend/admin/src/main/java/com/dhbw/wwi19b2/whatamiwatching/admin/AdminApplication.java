cfe2a0086c07d37146f854bb7c89de2ee7789781
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-05-16 12:23:19 +0200

    Add admin server

---
+++ backend/admin/src/main/java/com/dhbw/wwi19b2/whatamiwatching/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.dhbw.wwi19b2.whatamiwatching.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
