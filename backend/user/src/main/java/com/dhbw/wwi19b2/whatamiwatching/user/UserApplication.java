19918011a7d350fa7960b4984e60f7a4c6c3b558
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-05-17 14:48:42 +0200

    Create user server

---
+++ backend/user/src/main/java/com/dhbw/wwi19b2/whatamiwatching/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.wwi19b2.whatamiwatching.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
