224c637246e1b4eb93f9cdac663ef5066284026c
simonkritzer <simon.kritzer@hotmail.de>
2022-05-30 08:25:11 +0200

    Liste Versuch 2

--- frontend/src/screens/FilmList/index.js
+++ frontend/src/screens/FilmList/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { FlatList, Text, View } from 'react-native';
 import styles from './styles';
+import {ListItem} from "@rneui/themed";
 
-
+const list = [
+  {
+    name: 'Amy',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    subtitle: 'Vice Chairman'
+  }
+]
 
 const FilmListScreen = () => {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={[
-          {key: 'Avatar'},
-          {key: 'Star Wars'},
-          {key: 'Doctor Strange'},
-          {key: 'Iron Man'},
-          {key: 'Thor'},
-          {key: 'Captain America'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-      />
+      {
+        list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))
+      }
     </View>
   );
 }
