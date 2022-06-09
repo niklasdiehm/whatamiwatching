bd674a54517d18f4063e8a32ff7321128ba462da
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-06-09 09:50:43 +0200

    Changed user to mysql database

--- backend/user/src/main/java/com/dhbw/wwi19b2/whatamiwatching/user/UserApplication.java
+++ backend/user/src/main/java/com/dhbw/wwi19b2/whatamiwatching/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.dhbw.wwi19b2.whatamiwatching.user;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
