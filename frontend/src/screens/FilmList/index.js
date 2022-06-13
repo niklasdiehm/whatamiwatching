95771a34c9a313bb8199d9d3c2494d2e164836cc
simonkritzer <simon.kritzer@hotmail.de>
2022-06-13 15:35:03 +0200

    Ich bekomm hier garnichts hin, wieder 5h meines Lebens verschwendet

--- frontend/src/screens/FilmList/index.js
+++ frontend/src/screens/FilmList/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import styles from './styles';
 import { ListItem } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
@@ -11,19 +11,20 @@ const FilmListScreen = (route) => {
   const navigation = useNavigation();
   //const { userID } = route.params;
   const api2 = new api();
-  
-  /* async function getMovieOfTheDayList() {
-    list2 = [{adult, genre_ids: [], id, overview, release_date, title}] = await api2.getMovieOfTheDay(1);
+
+
+  async function getMovieOfTheDayList() {
+    list2 = [{ adults, genre_ids: [], id, overview, release_date, title }] = await Promise.all(api2.getMovieOfTheDay(1));
     console.log(list2)
-    
+
     return list2;
-  } */
+  }
 
 
   return (
     <View style={styles.container}>
-      { 
-        route.params.list1.map((l, i) => (
+      {
+        /* getMovieOfTheDayList().map((l, i) => (
           <ListItem key={i}
             bottomDivider
             onPress={() => navigation.navigate("FilmDetails", { movieID: l.id })}>
@@ -33,7 +34,22 @@ const FilmListScreen = (route) => {
             </ListItem.Content>
             <ListItem.Chevron color="black" />
           </ListItem>
-        ))
+        )) */
+        <FlatList
+        data={[
+          {key: getMovieOfTheDayList()[0]?.title || "not loaded yet"},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
       }
     </View>
   );
