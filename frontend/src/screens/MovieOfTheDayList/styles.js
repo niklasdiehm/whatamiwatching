001019f15eef08fde9d4525b122de4a451f63691
Marco-Lichtenberger <70747829+Marco-Lichtenberger@users.noreply.github.com>
2022-07-06 10:28:35 +0200

    style motd angepasst

--- frontend/src/screens/MovieOfTheDayList/styles.js
+++ frontend/src/screens/MovieOfTheDayList/styles.js
@@ -3,11 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    alignItems: "center",
+
     backgroundColor: "black",
   },
   item: {
-    padding: 10,
     fontSize: 18,
     height: 44,
     color: "white",
